13ac91fbafe654ab57e3f8226436f6154f11d855
ashmitiscodingpython <ashmit.jeevan@gmail.com>
2026-01-24 18:58:28 +0530

    Hourly commit - changed a bit in close script

--- scripts/close.gd
+++ scripts/close.gd
@@ -4,38 +4,31 @@ var mouse = false
 var inflate = false
 @export var black = false
 @export var factory = false
-@export var color = false
+@export var shop = false
+@export var settings = false
 @onready var area = $"."
 @onready var bla = $"../../Black Background"
 @onready var game_manager = %GameManager
+var norm = Vector2()
+
+func _ready() -> void:
+	norm = area.scale
 
 func _process(_delta: float) -> void:
 	if mouse:
-		if !factory:
-			var scaled = (1 - area.scale.x) / 5
-			area.scale += Vector2(scaled, scaled)
-		elif !color:
-			area.scale += (Vector2(0.86, -1.199) - area.scale) / 5
-		elif color:
-			area.scale += (Vector2(0.7728, -1.0776) - area.scale) / 5
+		area.scale += ((norm * 1.2) - area.scale) / 5
 	else:
-		if !factory:
-			var scaled = (0.8 - area.scale.x) / 5
-			area.scale += Vector2(scaled, scaled)
-		elif !color:
-			area.scale += (Vector2(0.67, -0.934) - area.scale) / 5
-		elif color:
-			area.scale += (Vector2(0.644, -0.898) - area.scale) / 5
+		area.scale += (norm - area.scale) / 5
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.pressed:
-		if mouse and factory and !color:
+		if mouse:
 			game_manager.facset = false
-		elif mouse and !black and !color:
+		elif mouse:
 			game_manager.open = false
-		elif mouse and black:
+		elif mouse:
 			game_manager.black = false
-		elif mouse and color:
+		elif mouse:
 			game_manager.factory_color_prompt = false
 		
 func _on_mouse_entered() -> void:
